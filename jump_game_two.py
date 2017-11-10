a417a201ac3d5f75b7484295ac9fdfad924fa864
minglu <goodlucklm@gmail.com>
2017-11-10 13:31:27 -0800

    added bfs and direct jump

--- jump_game_two.py
+++ jump_game_two.py
@@ -22,6 +22,48 @@ class JumpGameII(object):
         # dfs is good for searching the only path not the best path
         pass
 
+    def bfs(self, nums):
+        # peal the array level by level
+        level = [0]
+        count = 0
+        while len(nums)-1 not in level:
+            count += 1
+            next_level = set()
+            for i in level:
+                jump_distance = nums[i]
+                max_step = 0
+                next_jump = 0
+                for n in range(i+1, i +jump_distance+1):
+                    if n == len(nums)-1:
+                        return count
+                    if n < len(nums):
+                        if n+nums[n] > max_step:
+                            max_step = n+nums[n]
+                            next_jump = n
+                next_level.add(next_jump)
+            level = list(next_level)
+        return count
+
+    def direct_jump(self, nums):
+        # pick the next step that covers the most range
+        next_step = 0
+        count = 0
+        while next_step < len(nums)-1:
+            count += 1
+            jump_distance = nums[next_step]
+            max_step = 0
+            next_jump = 0
+            for n in range(next_step+1, next_step+jump_distance+1):
+                if n == len(nums)-1:
+                    return count
+                if n + nums[n] > max_step:
+                    max_step = n + nums[n]
+                    next_jump = n
+            next_step = next_jump
+        return count
+
+
+
     def jump(self, nums):
         """
         :type nums: List[int]
@@ -29,7 +71,7 @@ class JumpGameII(object):
         """
         if len(nums) < 2:
             return 0
-        return self.dfs(nums, 0)
+        return self.direct_jump(nums)
 
 
 if __name__ == '__main__':
