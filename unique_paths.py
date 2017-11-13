49b953cebff645f9670eab2d4dd9a0269770b78b
minglu <goodlucklm@gmail.com>
2017-11-13 14:03:45 -0800

    accepted

---
+++ unique_paths.py
@@ -0,0 +1,27 @@
+class UniquePaths(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = []
+        for i in range(m+1):
+            dp.append([0]*(n+1))
+
+        for i in range(m):
+            dp[i][0] = 0
+        for j in range(n):
+            dp[0][j] = 0
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if i == 1 and j == 1:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        return dp[m][n]
+
+if __name__ == '__main__':
+    up = UniquePaths()
+    print up.uniquePaths(3,7)
