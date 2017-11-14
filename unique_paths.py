a544e80360303dd67a9263de9fc5c59dde251500
minglu <goodlucklm@gmail.com>
2017-11-13 16:09:33 -0800

    remove unnecessary initialization

--- unique_paths.py
+++ unique_paths.py
@@ -9,11 +9,6 @@ class UniquePaths(object):
         for i in range(m+1):
             dp.append([0]*(n+1))
 
-        for i in range(m):
-            dp[i][0] = 0
-        for j in range(n):
-            dp[0][j] = 0
-
         for i in range(1, m+1):
             for j in range(1, n+1):
                 if i == 1 and j == 1:
