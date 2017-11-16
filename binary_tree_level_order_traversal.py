18bd4a82e75f35e854f6a6ccaaba4397b49d56e3
minglu <goodlucklm@gmail.com>
2017-11-16 13:15:02 -0800

    added bottomuptraversal, beats 79.4%

--- binary_tree_level_order_traversal.py
+++ binary_tree_level_order_traversal.py
@@ -16,8 +16,24 @@ class BinaryTreeLevelOrderTraversal(object):
         self.traverse(root, results, 0)
         return results
 
+    def bottomUPTraversal(self, root):
+        results = []
+        level = [root]
+        while len(level) > 0:
+            nodes = [node.val for node in level if node is not None]
+            if len(nodes) > 0:
+                results.insert(0, nodes)
+            n = len(level)
+            for i in range(n):
+                node = level[i]
+                if node is not None:
+                    level += [node.left, node.right]
+            level = level[n:]
+        return results
+
+
 if __name__ == '__main__':
     import tree_utilities
     root = tree_utilities.deserialize('[3,9,20,null,null,15,7]')
     solution = BinaryTreeLevelOrderTraversal()
-    print solution.levelOrder(root)
+    print solution.bottomUPTraversal(root)
