16952b06437076ada71ced8c5e07707e94fbd62c
minglu <goodlucklm@gmail.com>
2017-11-16 15:32:46 -0800

    combined zigzag into binary tree class

--- binary_tree.py
+++ binary_tree.py
@@ -1,4 +1,4 @@
-class BinaryTreeLevelOrderTraversal(object):
+class BinaryTree(object):
     def traverse(self, p, results, depth):
         if p is not None:
             self.traverse(p.left, results, depth+1)
@@ -31,9 +31,36 @@ class BinaryTreeLevelOrderTraversal(object):
             level = level[n:]
         return results
 
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        depth = 0
+        level = [root]
+        results = []
+        while len(level) > 0:
+            values = []
+            for node in level:
+                if node is not None:
+                    values.append(node.val)
+            if len(values) > 0:
+                if depth % 2 != 0:
+                    values.reverse()
+                results.append(values)
+
+            n = len(level)
+            for i in range(n):
+                node = level.pop(0)
+                if node is not None:
+                    level.append(node.left)
+                    level.append(node.right)
+            depth += 1
+        return results
+
 
 if __name__ == '__main__':
     import tree_utilities
     root = tree_utilities.deserialize('[3,9,20,null,null,15,7]')
-    solution = BinaryTreeLevelOrderTraversal()
+    solution = BinaryTree()
     print solution.bottomUPTraversal(root)
